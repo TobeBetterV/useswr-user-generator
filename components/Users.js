72d8d38e1f1493387fed986a92e8539ffa51ccca
tobebetterV <tobebetterV@users.noreply.github.com>
2022-08-05 14:06:46 +0800

    modified

---
+++ components/Users.js
@@ -0,0 +1,49 @@
+import axios from "axios";
+import useSWR from "swr";
+import Image from "next/image";
+
+export default function Users({count, setCount}) {
+  // const address = `https://randomuser.me/api/?results=6`;
+  const address = `https://randomuser.me/api/?results=${count}`;
+  const fetcher = async (url) => await axios.get(url).then((res) => res.data);
+  const { data, error } = useSWR(address, fetcher);
+  // const address2 = `https://randomuser.me/api/?results=${count+3}`;
+  // const { data1, error1 } = useSWR(address2, fetcher);
+
+  if (error) <p>Loading failed...</p>;
+  if (!data) <h1>Loading...</h1>;
+
+  return (
+    <div>
+      <div className="container">
+        {data &&
+          data.results.map((item) => (
+            <div key={item.cell} className={`user-card  ${item.gender}`}>
+              <div>
+                <Image
+                  width={100}
+                  height={100}
+                  src={item.picture.large}
+                  alt="user-avatar"
+                  className="img"
+                />
+                <h3>{`${item.name.first}  ${item.name.last}`}</h3>
+              </div>
+              <div className="details">
+                <p>Country: {item.location.country}</p>
+                <p>State: {item.location.state}</p>
+                <p>Email: {item.email}</p>
+                <p>Phone: {item.phone}</p>
+                <p>Age: {item.dob.age}</p>
+              </div>
+            </div>
+          ))}
+      </div>
+      <center>
+        <div className="btn">
+          <button onClick={() => setCount(count + 3)}>Load More Users</button>
+        </div>
+      </center>
+    </div>
+  );
+}
--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    domains: ["randomuser.me"],
+  },
+};
